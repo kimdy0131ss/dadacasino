e2b7cefafbcf4e9c8957afc9b1ea6010cf2bb191
kimdy0131ss <kimdy0131ss@gmail.com>
2025-10-25 19:59:39 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
+
+const app = express();
+const db = new sqlite3.Database("users.db");
+
+// CORS 설정
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+app.use(bodyParser.json());
+
+db.run(`CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  userbirth TEXT,
+  userid TEXT UNIQUE,
+  password TEXT
+)`);
+
+app.post("/signin", (req, res) => {
+  const { username, userbirth, userid, password } = req.body;
+  db.run(`INSERT INTO users (username, userbirth, userid, password) VALUES (?, ?, ?, ?)`, [username, userbirth, userid, password],
+    function(err) {
+        if (err) return res.send("이미 존재하는 아이디입니다.");
+        res.send("회원가입 성공!");
+    });
+});
+
+app.listen(3000, () => console.log("서버 실행 중"));
